19d4dc576db4f9129d83373504176a88cdfb72ae
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-02 14:13:00 +0530

    Rename package name

--- employeeportal/src/main/java/com/everest/employeeportal/config/JpaConfig.java
+++ employeeportal/src/main/java/com/everest/employeeportal/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.everest.employeeportal.com.everest.employeeportal.config;
+package com.everest.employeeportal.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
