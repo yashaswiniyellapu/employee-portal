a2e5a377b006658db6cc62d5d2951c1481a45374
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 19:34:13 +0530

    Delete employee

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.everest.employeeportal.controller;
 
-import com.everest.employeeportal.entities.Employee;
-import com.everest.employeeportal.exceptions.EmployeeNotFoundException;
-import com.everest.employeeportal.repository.EmployeeRepository;
+import com.everest.employeeportal.exceptions.EmptyDataException;
 import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -15,16 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeController {
     public final EmployeeService employeeService;
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee,
-                                                   @PathVariable(name = "id") Long empId)
-    {
-        Employee EMPLOYEE = employeeService.updateEmployee(employee, empId);
-        if(EMPLOYEE==null)
-        {
-            throw new EmployeeNotFoundException(empId);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable(name = "id") Long empId) {
+        try {
+            employeeService.deleteEmployee(empId);
+        } catch (Exception e) {
+            throw new EmptyDataException();
         }
-        return ResponseEntity.status(HttpStatus.FOUND).body(employee);
+        return ResponseEntity.status(HttpStatus.FOUND).body("deleted successfully");
     }
 
 }
