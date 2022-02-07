fa78f6c3705e03ec69856c544814c7be825a3e28
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 19:05:21 +0530

    Extend Jpa Repo

--- employeeportal/src/main/java/com/everest/employeeportal/repository/EmployeeRepository.java
+++ employeeportal/src/main/java/com/everest/employeeportal/repository/EmployeeRepository.java
@@ -1,43 +1,11 @@
 package com.everest.employeeportal.repository;
 
 import com.everest.employeeportal.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class EmployeeRepository {
-    private final AtomicLong NEXT_ID = new AtomicLong(0);
-    private final HashMap<Long, Employee> EMPLOYEES = new HashMap<>();
-
-    public List<Employee> getAllEmployees() {
-        return new ArrayList<>(EMPLOYEES.values());
-    }
-
-    public Employee getEmployeeByName() {
-        return null;
-    }
-
-    public List<Employee> getEmployeesSortNameByDateOfJoin() {
-        return null;
-    }
-
-    public Employee createEmployee(Employee employee) {
-        employee.setEmpId(NEXT_ID.getAndIncrement());
-        EMPLOYEES.put(employee.getEmpId(), employee);
-        return employee;
-    }
-
-    public Employee updateEmployee(Employee employee, Long emp_id) {
-        EMPLOYEES.put(emp_id, employee);
-        return employee;
-    }
-
-    public void deleteEmployee(Long employeeId) {
-        EMPLOYEES.remove(employeeId);
-    }
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
 }
