53fae3564cce4a563f01a7dc51ef5b50cbd907d9
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 14:13:28 +0530

    Remove unused imports

--- employeeportal/src/main/java/com/everest/employeeportal/repository/EmployeeRepository.java
+++ employeeportal/src/main/java/com/everest/employeeportal/repository/EmployeeRepository.java
@@ -4,10 +4,6 @@ import com.everest.employeeportal.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
