666d2c29fe4186ad0a44b0a5aa9fe108e76c670e
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 19:06:22 +0530

    Update employee update service

---
+++ employeeportal/src/main/java/com/everest/employeeportal/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.everest.employeeportal.services;
+
+import com.everest.employeeportal.entities.Employee;
+import com.everest.employeeportal.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public Employee updateEmployee(Employee employee, Long empId) {
+        employee.setEmpId(empId);
+        return employeeRepository.save(employee);
+    }
+}
