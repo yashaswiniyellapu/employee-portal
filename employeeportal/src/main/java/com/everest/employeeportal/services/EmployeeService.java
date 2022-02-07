b5eaf5ebb13c8d5df9b424e1a3d3cef16258062e
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 14:31:01 +0530

    Resolve can't instantiate employeerespository bean

--- employeeportal/src/main/java/com/everest/employeeportal/services/EmployeeService.java
+++ employeeportal/src/main/java/com/everest/employeeportal/services/EmployeeService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
     public List<Employee> fetchAllEmployees()
     {
         return employeeRepository.findAll();
