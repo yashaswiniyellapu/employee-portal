164237644f74ffed7c1059d53aeb546688878500
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-07 17:06:18 +0530

    fetch record by id

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -1,58 +1,32 @@
 package com.everest.employeeportal.controller;
 
 import com.everest.employeeportal.entities.Employee;
-import com.everest.employeeportal.repository.EmployeeRepository;
+import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
-    public final EmployeeRepository employeeRepository;
+    public final EmployeeService employeeService;
 
-    @GetMapping(value = "")
-    public List<Employee> getAllEmployees() {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Employee> getAllEmployees(@PathVariable("id") Long id) {
+        System.out.println("empid "+ id);
 
-        return employeeRepository.getAllEmployees();
-    }
-
-    @GetMapping(value = "/name")
-    public ResponseEntity<Employee> getEmployeesByName() {
-        final Employee employee = employeeRepository.getEmployeeByName();
-        if (employee == null) {
+        Employee EMPLOYEE = employeeService.fetchEmployeeById(id);
+        if (EMPLOYEE == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } else {
-            return ResponseEntity.status(HttpStatus.FOUND).body(employee);
+            return ResponseEntity.ok().body(EMPLOYEE);
         }
     }
 
-    @GetMapping(value = "/nameByDate")
-    public List<Employee> getEmployeesSortNameByDateOfJoin() {
-
-        return employeeRepository.getEmployeesSortNameByDateOfJoin();
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable("id") Long id) {
-
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(employeeRepository.updateEmployee(employee, id));
-    }
-
-    @PostMapping(value = "")
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(employeeRepository.createEmployee(employee));
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
-        employeeRepository.deleteEmployee(employeeId);
-       return ResponseEntity.status(HttpStatus.ACCEPTED).body("Deleted record") ;
-    }
 
 }
