9edf0f916c29db80422f1740fb5c64c5f3aa3f78
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-09 12:42:20 +0530

    Implement find by name feature

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -1,11 +1,9 @@
 package com.everest.employeeportal.controller;
 
 import com.everest.employeeportal.entities.Employee;
-import com.everest.employeeportal.repository.EmployeeRepository;
+import com.everest.employeeportal.exceptions.NameNotFoundException;
 import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +17,15 @@ public class EmployeeController {
     @GetMapping(value = "")
     public List<Employee> getAllEmployees() {
         return employeeService.fetchAllEmployees();
+    }
+
+    @GetMapping(value = "/{firstName}")
+    public Employee getEmployeeByName(@PathVariable(name="firstName")String firstName) {
+        Employee EMPLOYEE = employeeService.findByFirstName(firstName);
+        if(EMPLOYEE==null)
+        {throw new NameNotFoundException(firstName);
+        }
+        return EMPLOYEE;
     }
 
 }
