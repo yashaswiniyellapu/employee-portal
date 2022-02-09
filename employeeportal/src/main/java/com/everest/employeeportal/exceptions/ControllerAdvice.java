25b76a1380579f382ce2ba046013a65a19e10131
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-09 13:03:49 +0530

    Add body message in controller advice

--- employeeportal/src/main/java/com/everest/employeeportal/exceptions/ControllerAdvice.java
+++ employeeportal/src/main/java/com/everest/employeeportal/exceptions/ControllerAdvice.java
@@ -3,13 +3,21 @@ package com.everest.employeeportal.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
    @ExceptionHandler(NameNotFoundException.class)
-   public ResponseEntity<Object> employeeNameNotFound(Exception e)
+   public ResponseEntity<Object> employeeNameNotFound(Exception e, WebRequest r)
    {
-       return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+       Map<String,Object> body = new HashMap<>();
+       body.put("timestamp", LocalDateTime.now());
+       body.put("message",e.getMessage());
+       return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
    }
 }
