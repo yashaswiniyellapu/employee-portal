cb4b54e6834de379591aaa9e2a40f6f87166a97b
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-16 12:50:09 +0530

    Add pagination and exceptions

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -1,24 +1,26 @@
 package com.everest.employeeportal.controller;
 
 import com.everest.employeeportal.entities.Employee;
-import com.everest.employeeportal.repository.EmployeeRepository;
 import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/employees")
+@Validated
 @RequiredArgsConstructor
 public class EmployeeController {
     public final EmployeeService employeeService;
 
     @GetMapping(value = "")
-    public List<Employee> getAllEmployees() {
-        return employeeService.fetchAllEmployees();
+    public Page<Employee> getAllEmployees(@RequestParam(name="page")
+                                                      @Min(value=0, message="The page indexing from zero")
+                                                      int pageNumber) {
+        return employeeService.fetchAllEmployees(pageNumber);
     }
 
 }
--- employeeportal/src/main/java/com/everest/employeeportal/services/EmployeeService.java
+++ employeeportal/src/main/java/com/everest/employeeportal/services/EmployeeService.java
@@ -3,16 +3,20 @@ package com.everest.employeeportal.services;
 import com.everest.employeeportal.entities.Employee;
 import com.everest.employeeportal.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class EmployeeService {
+
     private final EmployeeRepository employeeRepository;
-    public List<Employee> fetchAllEmployees()
-    {
-        return employeeRepository.findAll();
+
+    @Transactional(readOnly = true)
+    public Page<Employee> fetchAllEmployees(int pageNumber) {
+        return employeeRepository.findAll(PageRequest.of(pageNumber, 10));
     }
 }
