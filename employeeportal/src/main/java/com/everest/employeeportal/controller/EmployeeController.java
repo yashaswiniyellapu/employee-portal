ddba1d1a67a0ec3b22cb30776f81e7f007ee2c31
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-16 13:22:05 +0530

    Add validation to input params

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -6,16 +6,23 @@ import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/employees")
+@Validated
 @RequiredArgsConstructor
 public class EmployeeController {
     public final EmployeeService employeeService;
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable("id") Long empId) {
+    public ResponseEntity<Employee> updateEmployee(@RequestBody @Valid Employee employee,
+                                                   @PathVariable("id") @Min(value = 1, message = "min value of id 1")
+                                                           Long empId) {
         Employee updatedEmployee = employeeService.updateEmployee(employee, empId);
         if (updatedEmployee == null) {
             throw new EmployeeNotFoundException(empId);
