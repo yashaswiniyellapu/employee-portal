d960f752e81ae96938c13c0b9f5046f5ac6ad109
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-16 11:38:38 +0530

    Add validation to path variable and handle exception

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -6,19 +6,24 @@ import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
+
 @RestController
 @RequestMapping("/api/employees")
+@Validated
 @RequiredArgsConstructor
 public class EmployeeController {
     public final EmployeeService employeeService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Employee> getAllEmployees(@PathVariable("id") Long id) {
+    public ResponseEntity<Employee> getAllEmployees(@PathVariable("id")
+                                                        @Min(value= 1, message = "Id min value is one") Long id) {
         Employee employee = employeeService.fetchEmployeeById(id);
         if (employee == null) {
             throw new EmployeeNotFoundException(id);
