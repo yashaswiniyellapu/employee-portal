babb5627db8026ca59036a67f3a188bd86a2f1ce
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-17 22:20:04 +0530

    Add custom pagination

--- employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
+++ employeeportal/src/main/java/com/everest/employeeportal/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.everest.employeeportal.controller;
 
 import com.everest.employeeportal.entities.Employee;
+import com.everest.employeeportal.entities.EmployeeResults;
 import com.everest.employeeportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Min;
 
@@ -17,10 +21,11 @@ public class EmployeeController {
     public final EmployeeService employeeService;
 
     @GetMapping(value = "")
-    public Page<Employee> getAllEmployees(@RequestParam(name="page", required = false, defaultValue = "1")
-                                                      @Min(value=1, message="The page indexing from zero")
-                                                      int pageNumber) {
-        return employeeService.fetchAllEmployees(pageNumber);
+    public EmployeeResults getAllEmployees(@RequestParam(name = "page", required = false, defaultValue = "1")
+                                           @Min(value = 1, message = "The page indexing from zero")
+                                                   int pageNumber) {
+        Page<Employee> paginatedEmployees = employeeService.fetchAllEmployees(pageNumber);
+        return new EmployeeResults(paginatedEmployees);
     }
 
 }
