2f8eadee95f3ea170eaaa2cb45574a7f55467d98
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-17 12:38:29 +0530

    Change message

--- employeeportal/src/main/java/com/everest/employeeportal/exceptions/EmptyDataException.java
+++ employeeportal/src/main/java/com/everest/employeeportal/exceptions/EmptyDataException.java
@@ -3,6 +3,6 @@ package com.everest.employeeportal.exceptions;
 public class EmptyDataException extends  RuntimeException {
     public EmptyDataException(Long empId)
     {
-        super("Record not with id "+empId+" found");
+        super("Record with employee id "+empId+" not found");
     }
 }
