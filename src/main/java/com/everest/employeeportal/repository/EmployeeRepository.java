bcdc180dccb1b2472165de73ba5ac861712d668c
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-21 14:03:46 +0530

    Change files to root folder

---
+++ src/main/java/com/everest/employeeportal/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.everest.employeeportal.repository;
+
+import com.everest.employeeportal.entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Boolean existsByEverestEmailId(String emailId);
+    @Query("select e from Employee e where firstName like %?1% or lastName like %?1%")
+    Page<Employee> findByName(String name, Pageable pageable);
+
+}
+
+
