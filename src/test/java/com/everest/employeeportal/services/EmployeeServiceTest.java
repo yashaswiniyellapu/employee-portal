1c8dd17c749b09958968fb7d1f98737c6a3e9698
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-02-22 12:11:12 +0530

    Add getAllPaginatedEmployees test case

--- src/test/java/com/everest/employeeportal/services/EmployeeServiceTest.java
+++ src/test/java/com/everest/employeeportal/services/EmployeeServiceTest.java
@@ -7,6 +7,12 @@ import com.everest.employeeportal.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,41 +23,54 @@ class EmployeeServiceTest {
 
     private EmployeeRepository employeeRepo;
     private EmployeeService employeeService;
+    private Employee employee;
 
     @BeforeEach
     void setup() {
         employeeRepo = Mockito.mock(EmployeeRepository.class);
         employeeService = new EmployeeService(employeeRepo);
-    }
-
-    @Test
-    void shouldCreateEmployeeSuccessfully() {
-        Employee employee = new Employee(null, "yashu", null,
+        employee=new Employee(null, "yashu", null,
                 "yashu@Everest", null, null, null,
                 "trainee", 0, "good",
                 new Address(null, "ATPPreLine1", "ATPPreLine2",
                         "guljarPet", "Andhra", 515001, "India"),
                 new Address(null, "ATPPerLine1", "ATPPerLine2",
                         "guljarPet", "AndhraPradesh", 515002, "India"));
+    }
+
+    @Test
+    void shouldCreateEmployeeSuccessfully() {
+        //arranging data
         when(employeeRepo.existsByEverestEmailId("yashu@Everest")).thenReturn(false);
         when(employeeRepo.save(employee)).then(invocation -> invocation.getArgument(0));
+        //operations on data
         Employee savedEmployee = employeeService.createEmployee(employee);
+        //asserting on service layer
         assertThat(savedEmployee).isNotNull();
         verify(employeeRepo).save(any(Employee.class));
     }
     @Test
     void shouldThrowErrorWhenSavingEmployeeWithExistingEverestEmailId() {
-        Employee employee = new Employee(null, "yashu", null,
-                "y@everest.com", null, null, null,
-                "trainee", 0, "good",
-                new Address(null, "ATPPreLine1", "ATPPreLine2",
-                        "guljarPet", "Andhra", 515001, "India"),
-                new Address(null, "ATPPerLine1", "ATPPerLine2",
-                        "guljarPet", "AndhraPradesh", 515002, "India"));
-        when(employeeRepo.existsByEverestEmailId("y@everest.com")).thenReturn(true);
+        when(employeeRepo.existsByEverestEmailId("yashu@Everest")).thenReturn(true);
         assertThrows(EmployeeAlreadyExistsException.class,()->{
             employeeService.createEmployee(employee);});
         verify(employeeRepo,never()).save(any(Employee.class));
 
     }
+    @Test
+    void shouldGetAllPaginatedEmployees()
+    {
+        //arranging data
+        List<Employee> employeeData= List.of(employee);
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Employee> paginatedEmployees = new PageImpl<>(employeeData,pageable,1);
+        when(employeeRepo.findAll(pageable)).thenReturn(paginatedEmployees);
+        //operations on data
+        Page<Employee> employeeList= employeeService.findAllEmployees(1);
+        //asserting data
+        assertThat(employeeList.getTotalElements()).isEqualTo(1);
+        verify(employeeRepo).findAll(any(Pageable.class));
+    }
+
+
 }
