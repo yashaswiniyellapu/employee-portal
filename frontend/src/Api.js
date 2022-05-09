ddc7e603460f239d992472f8b08cfa50b55d07b6
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-05-09 14:08:02 +0530

    Connect to ec2

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,29 +1,29 @@
 import axios from "axios";
-var config = {
-    headers: { "Content-Type":"application/json",
-"Access-Control-Allow-Origin":"*"},
-  };
-const API_BASE_URL = 'http://localhost:8080'
+//var config = {
+//    headers: { "Content-Type":"application/json",
+//"Access-Control-Allow-Origin":"*",
+//  };
+const API_BASE_URL = 'http://13.233.117.57:8080'
 
 export function fetchEmployees() {
-    return axios.get(`${API_BASE_URL}/api/employees`,config);
+    return axios.get(`${API_BASE_URL}/api/employees`);
 }
 
 export function createEmployee(employee) {
-    return axios.post(`${API_BASE_URL}/api/employees`, employee,config);
+    return axios.post(`${API_BASE_URL}/api/employees`, employee);
 }
 
 export function deleteEmployee(id) {
-    return axios.delete(`${API_BASE_URL}/api/employees/${id}`,config);
+    return axios.delete(`${API_BASE_URL}/api/employees/${id}`);
 }
 
 export function fetchEmployeeById(id) {
-    return axios.get(`${API_BASE_URL}/api/employees/${id}`,config);
+    return axios.get(`${API_BASE_URL}/api/employees/${id}`);
 }
 
 export function updateEmployee(id,employee) {
-    return axios.delete(`${API_BASE_URL}/api/employees/${id}`,employee,config);
+    return axios.delete(`${API_BASE_URL}/api/employees/${id}`,employee);
 }
 export function searchByName(name) {
-    return axios.get(`${API_BASE_URL}/api/employees/search?query=${name}`,config);
+    return axios.get(`${API_BASE_URL}/api/employees/search?query=${name}`);
 }
