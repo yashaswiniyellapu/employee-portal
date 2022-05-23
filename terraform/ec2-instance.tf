506132db4baf1e127925388e1658505b7f764d47
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-05-23 19:12:29 +0530

    update provider with version

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -1,12 +1,3 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 provider "aws" {
       region = "us-east-1"
       access_key = "AKIA55FVHWUZN4NXLKU4"
