eef3fce4194e2721f4b74734790029de668a3770
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-05-24 11:17:03 +0530

    test apply workflow

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -1,3 +1,12 @@
+variable "aws_access_key" {
+ type = string  
+}
+variable "aws_secret_key" {
+ type = string  
+}
+variable "aws_region" {
+ type = string  
+}
 provider "aws" {
       region = var.aws_region
       access_key = var.aws_access_key
